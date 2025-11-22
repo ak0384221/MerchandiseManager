ea170826c23837985193b3eecb340c219f1616e4
Ayaan <sharingan0384221@gmail.com>
2025-11-22 20:45:17 +0600

    fixed the see as admin/user button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 
 export default function Header({ Heading }) {
   return (
-    <nav className=" w-full print:hidden text-center  pt-6 mb-4 flex justify-center items-center gap-2">
-      <img src="/logo2.png" className="size-[2rem]  " alt="" />{" "}
-      <h1 className="logo  text-4xl font-Aladin text-center  ">{Heading}</h1>
-      <button className="  absolute top-0 right-0 m-4 font-bold  text-white px-2 py-1 text-center  hover:bg-neutral-800 space-x-2  border border-neutral-500 rounded-md">
+    <>
+      <button className="  w-full  bg-[#1f2124] text-white font-bold  px-2 py-2 text-center  hover:bg-neutral-800 space-x-2  ">
         <Link to={`${Heading === "M-Pannel" ? "/" : "/secret/alahomora"}`}>
-          <span className="text-sm capitalize">
-            {Heading === "M-Pannel" ? "Visit User Page" : "Visit Admin Page"}
+          <span className="text-xs capitalize">
+            {Heading === "M-Pannel" ? "Use as User" : "Use as Admin"}
           </span>
         </Link>
       </button>
-    </nav>
+      <nav className=" w-full print:hidden text-center  pt-6 mb-4 flex justify-center items-center gap-2">
+        <img src="/logo2.png" className="size-[2rem]  " alt="" />{" "}
+        <h1 className="logo  text-4xl font-Aladin text-center  ">{Heading}</h1>
+      </nav>
+    </>
   );
 }
